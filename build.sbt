0e7a29f66b15044b016692c99b0c4ef86f54e91f
Andres Ventura <andres.ventura@itv.com>
2013-09-30 12:54:06 +0100

    s-99 to ex10

---
+++ build.sbt
@@ -0,0 +1,8 @@
+scalaVersion := "2.10.2"
+
+name := "S-99"
+
+version := "0.1"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.9.1" % "test"
+
---
+++ src/main/scala/ex/Exercises.scala
@@ -0,0 +1,59 @@
+package ex
+
+object Exercises {
+
+  def last[A](list: List[A]): A = list match {
+    case h :: Nil  => h
+    case _ :: tail => last(tail)
+    case _         => throw new NoSuchElementException
+  }
+
+  def penultimate[A](list: List[A]): A = list match {
+    case head :: last :: Nil => head
+    case head :: tail        => penultimate(tail)
+    case _                   => throw new NoSuchElementException
+  }
+
+  def nth[A](idx: Int, list: List[A]): A = idx match {
+    case 0        => list.head
+    case x        => nth(x - 1, list.tail)
+  }
+
+  def length[A](list: List[A]): Int = list match {
+    case Nil             => 0
+    case head :: tail    => 1 + length(tail)
+  }
+
+  def reverse[A](list: List[A]): List[A] = list match {
+    case Nil             =>  Nil
+    case head :: tail    => reverse(tail) ::: head :: Nil
+  }
+
+  def isPalindrome[A](list: List[A]): Boolean = list == list.reverse
+
+  def flatten(list: List[Any]): List[Any] = list match {
+    case Nil => Nil
+    case head :: tail => { head match {
+      case aList: List[Any] => flatten(aList)
+      case anElement: Any => anElement :: Nil
+    }} ::: flatten(tail)
+  }
+
+  def compress(list: List[Symbol]): List[Symbol] = list match {
+    case Nil => Nil
+    case last :: Nil => last :: Nil
+    case head :: tail if head == tail.head => compress(tail)
+    case head :: tail                      => head :: compress(tail)
+  }
+
+  def pack(list: List[Symbol]): List[Any] = list match {
+    case Nil => Nil
+    case head :: tail => List(head :: tail.takeWhile(_ == head)) ::: pack(tail.dropWhile(_ == head))
+  }
+
+  def encode(list: List[Symbol]): List[(Int, Symbol)] = list match {
+    case Nil => Nil
+    case head :: tail => List((tail.takeWhile(_ == head).size + 1, head)) ::: encode(tail.dropWhile(_ == head))
+  }
+
+}
