bd6c14e90efec33f77e32a7f9adb7b1831fb7134
Andres Ventura <andres.ventura@itv.com>
2013-09-30 12:54:06 +0100

    s-99 to ex10

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-S-99
-====
-
-S-99: Ninety-Nine Scala Problems
+http://aperiodic.net/phil/scala/s-99/
---
+++ src/test/scala/exc/ExercisesTest.scala
@@ -0,0 +1,55 @@
+package exc
+
+import org.scalatest.{BeforeAndAfter, FunSuite}
+
+class ExercisesTest extends FunSuite with BeforeAndAfter {
+
+  test("Test P01") {
+    assert(Exercises.last(List(1, 1, 2, 3, 5, 8)) === 8)
+  }
+
+  test("Test P02") {
+    assert(Exercises.penultimate(List(1, 1, 2, 3, 5, 8)) === 5)
+  }
+
+  test("Test P03") {
+    assert(Exercises.nth(2, List(1, 1, 2, 3, 5, 8)) === 2)
+  }
+
+  test("Test P04") {
+    assert(Exercises.length(List(1, 1, 2, 3, 5, 8)) === 6)
+  }
+
+  test("Test P05") {
+    assert(Exercises.reverse(List(1, 2, 3, 4, 5, 6)) === List(6, 5, 4, 3, 2, 1))
+  }
+
+  test("Test P06") {
+    assert(Exercises.isPalindrome(List(1, 2, 3, 2, 1)) === true)
+  }
+
+  test("Test P07") {
+    assert(Exercises.flatten(List(List(1, 1), 2, List(3, List(5, 8)))) === List(1, 1, 2, 3, 5, 8))
+  }
+
+  test("Test P08") {
+    assert(Exercises.compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) === List('a, 'b, 'c, 'a, 'd, 'e))
+    assert(Exercises.compress(Nil) === Nil)
+
+  }
+
+  test("Test P09") {
+    assert(Exercises.pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) ===
+      List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e)))
+    assert(Exercises.pack(List('a)) ===
+      List(List('a)))
+  }
+
+  test("Test P10") {
+    assert(Exercises.encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) ===
+      List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e)))
+  }
+
+
+
+}
